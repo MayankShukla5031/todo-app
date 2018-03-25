c5ab6c2dc35b66f4b586088f003e2281ce6eff4c
mayank <mayank@vernacular.ai>
2018-03-26 01:22:07 +0530

    feat: clearing the store data after logout action
    >> using rootreducer concept to clear the store data when user logged out successfully

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -13,11 +13,11 @@ export const handleLogin = data => dispatch => {
   apiCalls.postCall(LOGIN, data)
     .then(res => {
       if (res && res.status === 200) {
+        localStorage.setItem('user_id', res.data.user_id)
         dispatch({
           type: actionTypes.LOGIN_SUCCESS,
           payload: { data: res.data },
         })
-        localStorage.setItem('user_id', res.data.user_id)
       }
     })
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,19 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import combinedReducers from './combinedReducers'
 
-const store = createStore(combinedReducers, applyMiddleware(thunk))
+/*
+  rootReducer is required to clear all the redux store data once user is logged-out,
+  data of one user should not be visible to any other user
+*/
+const rootReducer = (state, action) => {
+  let isReset = false
+  if (action.type === 'LOGOUT_SUCCESS') {
+    isReset = true
+  }
+  // whenever we return state value 'undefined', all the reducer will get reset to initial state
+  return combinedReducers(isReset ? undefined : state, action)
+}
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 export default store
